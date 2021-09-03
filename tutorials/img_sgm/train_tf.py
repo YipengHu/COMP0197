a0aa483e20f5ec057a1f68403526446779f185e5
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2021-09-04 00:06:58 +0100

    ref #3 refactored train script

--- tutorials/img_sgm/train_tf.py
+++ tutorials/img_sgm/train_tf.py
@@ -2,228 +2,89 @@
 # adapted from: https://www.tensorflow.org/tutorials/images/segmentation
 
 
+import os
+
 import tensorflow as tf
-import utils
+from PIL import Image
+
+from loader import H5ImageLoader
+from network_tf import ResUNet
+import utils_tf as utils
 
 
 DATA_PATH = './data'
 
-# example images
-loader_train = utils.H5ImageLoader(DATA_PATH+'/images_train.h5', 20, DATA_PATH+'/labels_train.h5')
+
+## example train images
+images,labels = next(iter(H5ImageLoader(DATA_PATH+'/images_train.h5', 10, DATA_PATH+'/labels_train.h5')))
+image_montage = Image.fromarray(tf.concat([images[i] for i in range(len(images))],axis=1).numpy())
+image_montage.save("train_images.jpg")
+label_montage = Image.fromarray(tf.concat([labels[i] for i in range(len(labels))],axis=1).numpy())
+label_montage.save("train_labels.jpg")
+
+## settings
+minibatch_size = 20
+network_size = 16
+learning_rate = 1e-4
+num_epochs = 500
+freq_info = 1
+freq_save = 50
+save_path = "results_tf"
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
 
 
-for idx, (images,labels) in enumerate(loader_train):
-  print(idx)
+## data loader
+loader_train = H5ImageLoader(DATA_PATH+'/images_train.h5', minibatch_size, DATA_PATH+'/labels_train.h5')
+loader_val = H5ImageLoader(DATA_PATH+'/images_val.h5', 20, DATA_PATH+'/labels_val.h5')
 
 
-def normalize(input_image, input_mask):
-  input_image = tf.cast(input_image, tf.float32) / 255.0
-  input_mask -= 1
-  return input_image, input_mask
+## network
+seg_net = ResUNet(init_ch=network_size)
+seg_net = seg_net.build(input_shape=loader_train.image_size)
+# seg_net.summary()
 
+## train
+optimizer = tf.optimizers.Adam(learning_rate)
 
-def load_image(datapoint):
-  input_image = tf.image.resize(datapoint['image'], (128, 128))
-  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+@tf.function
+def train_step(images, labels):  # train step
+    with tf.GradientTape() as tape:
+        images, labels = utils.random_image_label_transform(images, labels)
+        predicts = seg_net(images, training=True)
+        loss = tf.reduce_mean(utils.dice_loss(predicts, labels))
+    gradients = tape.gradient(loss, seg_net.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, seg_net.trainable_variables))
+    return loss
 
-  input_image, input_mask = normalize(input_image, input_mask)
+@tf.function
+def val_step(images, labels):  # validation step
+    predicts = seg_net(images, training=False)
+    losses = utils.dice_loss(predicts, labels)
+    dices, false_positives = utils.dice_metric_fg(predicts, labels)
+    return losses, dices, false_positives
 
-  return input_image, input_mask
+for epoch in range(num_epochs):
 
+    for frames, masks in loader_train: 
+        loss_train = train_step(frames, masks)
 
-TRAIN_LENGTH = info.splits['train'].num_examples
-BATCH_SIZE = 64
-BUFFER_SIZE = 1000
-STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
+    if (epoch+1) % freq_info == 0:
+        tf.print('Epoch {}: loss={:0.5f}'.format(epoch,loss_train))
 
-train_images = dataset['train'].map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
-test_images = dataset['test'].map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
-
-class Augment(tf.keras.layers.Layer):
-  def __init__(self, seed=42):
-    super().__init__()
-    # both use the same seed, so they'll make the same randomn changes.
-    self.augment_inputs = preprocessing.RandomFlip(mode="horizontal", seed=seed)
-    self.augment_labels = preprocessing.RandomFlip(mode="horizontal", seed=seed)
-
-  def call(self, inputs, labels):
-    inputs = self.augment_inputs(inputs)
-    labels = self.augment_labels(labels)
-    return inputs, labels
-
-train_batches = (
-    train_images
-    .cache()
-    .shuffle(BUFFER_SIZE)
-    .batch(BATCH_SIZE)
-    .repeat()
-    .map(Augment())
-    .prefetch(buffer_size=tf.data.AUTOTUNE))
-
-test_batches = test_images.batch(BATCH_SIZE)
-
-def display(display_list):
-  plt.figure(figsize=(15, 15))
-
-  title = ['Input Image', 'True Mask', 'Predicted Mask']
-
-  for i in range(len(display_list)):
-    plt.subplot(1, len(display_list), i+1)
-    plt.title(title[i])
-    plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
-    plt.axis('off')
-    plt.show()
-
-for images, masks in train_batches.take(2):
-  sample_image, sample_mask = images[0], masks[0]
-  display([sample_image, sample_mask])
-
-## encoder-decoder
-base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
-
-# Use the activations of these layers
-layer_names = [
-    'block_1_expand_relu',   # 64x64
-    'block_3_expand_relu',   # 32x32
-    'block_6_expand_relu',   # 16x16
-    'block_13_expand_relu',  # 8x8
-    'block_16_project',      # 4x4
-]
-base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
-
-# Create the feature extraction model
-down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs)
-
-down_stack.trainable = False
-
-up_stack = [
-    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
-    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
-    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-    pix2pix.upsample(64, 3),   # 32x32 -> 64x64
-]
-
-def unet_model(output_channels:int):
-  inputs = tf.keras.layers.Input(shape=[128, 128, 3])
-
-  # Downsampling through the model
-  skips = down_stack(inputs)
-  x = skips[-1]
-  skips = reversed(skips[:-1])
-
-  # Upsampling and establishing the skip connections
-  for up, skip in zip(up_stack, skips):
-    x = up(x)
-    concat = tf.keras.layers.Concatenate()
-    x = concat([x, skip])
-
-  # This is the last layer of the model
-  last = tf.keras.layers.Conv2DTranspose(
-      filters=output_channels, kernel_size=3, strides=2,
-      padding='same')  #64x64 -> 128x128
-
-  x = last(x)
-
-  return tf.keras.Model(inputs=inputs, outputs=x)
-
-
-## compile with loss and optimiser
-OUTPUT_CLASSES = 3
-
-model = unet_model(output_channels=OUTPUT_CLASSES)
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-tf.keras.utils.plot_model(model, show_shapes=True)
-
-def create_mask(pred_mask):
-  pred_mask = tf.argmax(pred_mask, axis=-1)
-  pred_mask = pred_mask[..., tf.newaxis]
-  return pred_mask[0]
-
-def show_predictions(dataset=None, num=1):
-  if dataset:
-    for image, mask in dataset.take(num):
-      pred_mask = model.predict(image)
-      display([image[0], mask[0], create_mask(pred_mask)])
-  else:
-    display([sample_image, sample_mask,
-             create_mask(model.predict(sample_image[tf.newaxis, ...]))])
-
-show_predictions()
-
-class DisplayCallback(tf.keras.callbacks.Callback):
-  def on_epoch_end(self, epoch, logs=None):
-    clear_output(wait=True)
-    show_predictions()
-    print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
-
-EPOCHS = 20
-VAL_SUBSPLITS = 5
-VALIDATION_STEPS = info.splits['test'].num_examples//BATCH_SIZE//VAL_SUBSPLITS
-
-model_history = model.fit(train_batches, epochs=EPOCHS,
-                          steps_per_epoch=STEPS_PER_EPOCH,
-                          validation_steps=VALIDATION_STEPS,
-                          validation_data=test_batches,
-                          callbacks=[DisplayCallback()])
-
-loss = model_history.history['loss']
-val_loss = model_history.history['val_loss']
-
-plt.figure()
-plt.plot(model_history.epoch, loss, 'r', label='Training loss')
-plt.plot(model_history.epoch, val_loss, 'bo', label='Validation loss')
-plt.title('Training and Validation Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss Value')
-plt.ylim([0, 1])
-plt.legend()
-plt.show()
-
-show_predictions(test_batches, 3)
-
-## train 
-try:
-  model_history = model.fit(train_batches, epochs=EPOCHS,
-                            steps_per_epoch=STEPS_PER_EPOCH,
-                            class_weight = {0:2.0, 1:2.0, 2:1.0})
-  assert False
-except Exception as e:
-  print(f"{type(e).__name__}: {e}")
-
-label = [0,0]
-prediction = [[-3., 0], [-3, 0]] 
-sample_weight = [1, 10] 
-
-loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True,
-                                               reduction=tf.losses.Reduction.NONE)
-loss(label, prediction, sample_weight).numpy()
-
-def add_sample_weights(image, label):
-  # The weights for each class, with the constraint that:
-  #     sum(class_weights) == 1.0
-  class_weights = tf.constant([2.0, 2.0, 1.0])
-  class_weights = class_weights/tf.reduce_sum(class_weights)
-
-  # Create an image of `sample_weights` by using the label at each pixel as an 
-  # index into the `class weights` .
-  sample_weights = tf.gather(class_weights, indices=tf.cast(label, tf.int32))
-
-  return image, label, sample_weights
-
-train_batches.map(add_sample_weights).element_spec
-weighted_model = unet_model(OUTPUT_CLASSES)
-weighted_model.compile(
-    optimizer='adam',
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy'])
-weighted_model.fit(
-    train_batches.map(add_sample_weights),
-    epochs=1,
-    steps_per_epoch=10)
-
-# save trained model
-model.save('saved_model_tf')
-print('Model saved.')
+    if (epoch+1) % freq_save == 0:
+        losses_all, dices_all, false_positives_all = [], [], []
+        for frames_val, masks_val in loader_val:
+            losses, dices, false_positives = val_step(frames_val, masks_val)
+            losses_all += [losses]
+            dices_all += [dices]
+            false_positives_all += [false_positives]
+        tf.print('Epoch {}: val-loss={:0.5f}, val-dice={:0.5f}, false_positives={:0.5f}'.format(
+            epoch,
+            tf.reduce_mean(tf.concat(losses_all,axis=0)),
+            tf.reduce_mean(tf.concat(dices_all,axis=0)),
+            tf.reduce_mean(tf.concat(false_positives_all,axis=0))
+            ))
+        tf.saved_model.save(seg_net, os.path.join(save_path, 'epoch{:d}'.format(epoch)))
+        tf.print('Model saved.')
