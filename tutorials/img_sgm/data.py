06ad0c481aca0c71b30cd09439cc45c0d7663473
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2021-09-03 12:04:48 +0100

    ref #3 data split added

--- tutorials/img_sgm/data.py
+++ tutorials/img_sgm/data.py
@@ -5,6 +5,8 @@
 import os
 import requests
 import tarfile
+import shutil
+import random
 
 from PIL import Image
 import numpy as np
@@ -13,16 +15,18 @@ import h5py
 
 DATA_PATH = './data'
 
-if not os.path.exists(DATA_PATH):
-    os.makedirs(DATA_PATH)
-
-
+## download
 filenames = ['images.tar.gz', 'annotations.tar.gz']
+url_base = 'https://www.robots.ox.ac.uk/~vgg/data/pets/data/'
+
+if os.path.exists(DATA_PATH):
+    shutil.rmtree(DATA_PATH)
+os.makedirs(DATA_PATH)
 
 print('Downloading and extracting data...')
 for temp_file in filenames:
-    url = 'https://www.robots.ox.ac.uk/~vgg/data/pets/data/' + temp_file
-    print('from ' + url + ' ...')
+    url = url_base + temp_file
+    print(url + ' ...')
     r = requests.get(url,allow_redirects=True)
     _ = open(temp_file,'wb').write(r.content)
     with tarfile.open(temp_file) as tar_obj:
@@ -31,23 +35,54 @@ for temp_file in filenames:
     os.remove(temp_file)
 
 
-print('Converting data...')
-image_dir = 'images'
+## spliting and converting
+img_dir = 'images'
 seg_dir = 'annotations/trimaps'
-im_size = (200,200)
+#----- options -----
+im_size = (128,128)
+ratio_val = 0.1
+ratio_test = 0.2
+#-------------------
+img_h5s, seg_h5s = [], []
+for s in ["train", "val", "test"]:
+    img_h5s.append(h5py.File(os.path.join(DATA_PATH,"images_{:s}.h5".format(s)), "w"))
+    seg_h5s.append(h5py.File(os.path.join(DATA_PATH,"labels_{:s}.h5".format(s)), "w"))
+
+img_filenames = [f for f in os.listdir(img_dir) if f.endswith('.jpg')]
+num_data = len(img_filenames)
+num_val = int(num_data * ratio_val)
+num_test = int(num_data * ratio_test)
+num_train = num_data - num_val - num_test
+
+print("Extracting data into %d-%d-%d for train-val-test (%0.2f-%0.2f-%0.2f)..." % (num_train,num_val,num_test, 1-ratio_val-ratio_test,ratio_val,ratio_test))
+
+random.seed(90)
+random.shuffle(img_filenames)
+
 # write all images/labels to h5 file
-img_h5 = h5py.File(os.path.join(DATA_PATH,"images.h5"), "w")
-seg_h5 = h5py.File(os.path.join(DATA_PATH,"labels.h5"), "w")
-for idx, im_file in enumerate([f for f in os.listdir(image_dir) if f.endswith('.jpg')]):
-    with Image.open(os.path.join(image_dir,im_file)) as img:
-        img = np.array(img.resize(im_size).getdata(),dtype='uint8').reshape(im_size[0],im_size[1],3)
-        img_h5.create_dataset("/{:06d}/".format(idx), data=img)
+for idx, im_file in enumerate(img_filenames):
+
+    if idx < num_train:  # train
+        ids = 0
+    elif idx < (num_train + num_val):  # val
+        ids = 1
+    else:  # test
+        ids = 2
+
+    with Image.open(os.path.join(img_dir,im_file)) as img:
+        img = np.array(img.convert('RGB').resize(im_size).getdata(),dtype='uint8').reshape(im_size[0],im_size[1],3)
+        img_h5s[ids].create_dataset("{:06d}".format(idx), data=img)
     with Image.open(os.path.join(seg_dir,im_file.split('.')[0]+'.png')) as seg:
         seg = np.array(seg.resize(im_size).getdata(),dtype='uint8').reshape(im_size[0],im_size[1])
-        seg_h5.create_dataset("/{:06d}/".format(idx), data=seg)
-img_h5.flush()
-seg_h5.flush()
-img_h5.close()
-seg_h5.close()
+        seg_h5s[ids].create_dataset("{:06d}".format(idx), data=seg)
+
+for ids in range(len(img_h5s)):
+    img_h5s[ids].flush()
+    img_h5s[ids].close()
+    seg_h5s[ids].flush()
+    seg_h5s[ids].close()
+
+shutil.rmtree(img_dir)
+shutil.rmtree(seg_dir.split('/')[0]) #remove entire annatations folder
 
 print('Data saved in %s.' % os.path.abspath(DATA_PATH))
--- tutorials/img_sgm/readme.md
+++ tutorials/img_sgm/readme.md
@@ -1,11 +1,3 @@
 # Image Segmentation
 
-For both PyTorch and TensorFlow scripts, the [TensorFlow Datasets](https://www.tensorflow.org/datasets) is required. Instal, e.g. in the conda environment:
-
-``` bash
-conda install -c anaconda tensorflow-datasets 
-```
-or 
-``` bash  
-pip install tfds-nightly  # pip install tensorflow-datasets
-```
+A common data source and loader will be used in this tutorial between PyTorch and TensorFlow.
