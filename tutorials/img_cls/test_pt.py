7402d0748dbe17f6c93f24fe14cb1cd244934e4a
Greenfrogs <5961364+greenfrogs@users.noreply.github.com>
2021-10-06 12:09:29 +0100

    add support for pytorch on Windows

--- tutorials/img_cls/test_pt.py
+++ tutorials/img_cls/test_pt.py
@@ -8,33 +8,34 @@ from PIL import Image
 
 from network_pt import Net
 
-## cifar-10 dataset
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+if __name__ == '__main__':
+    ## cifar-10 dataset
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-batch_size = 4
+    batch_size = 4
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
-dataiter = iter(testloader)
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
-## load the trained model
-model = Net()
-model.load_state_dict(torch.load('saved_model.pt'))
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    dataiter = iter(testloader)
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
-## inference
-images, labels = dataiter.next()
-print('Ground-truth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    ## load the trained model
+    model = Net()
+    model.load_state_dict(torch.load('saved_model.pt'))
 
-outputs = model(images)
-_, predicted = torch.max(outputs, 1)
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
 
-# save to images
-im = Image.fromarray((torch.cat(images.split(1,0),3).squeeze()/2*255+.5*255).permute(1,2,0).numpy().astype('uint8'))
-im.save("test_pt_images.jpg")
-print('test_pt_images.jpg saved.')
+    ## inference
+    images, labels = dataiter.next()
+    print('Ground-truth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+    outputs = model(images)
+    _, predicted = torch.max(outputs, 1)
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+    # save to images
+    im = Image.fromarray((torch.cat(images.split(1,0),3).squeeze()/2*255+.5*255).permute(1,2,0).numpy().astype('uint8'))
+    im.save("test_pt_images.jpg")
+    print('test_pt_images.jpg saved.')
