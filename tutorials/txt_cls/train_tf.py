693a3cd0866467ae19caa3686cc1bd51e53a6fe5
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2025-01-14 14:21:44 +0000

    ref #6 updated experimental keras features for new tf versions

--- tutorials/txt_cls/train_tf.py
+++ tutorials/txt_cls/train_tf.py
@@ -30,7 +30,7 @@ for example, label in train_dataset.take(1):
 
 ## text encoder
 VOCAB_SIZE = 1000
-encoder = tf.keras.layers.experimental.preprocessing.TextVectorization(
+encoder = tf.keras.layers.TextVectorization(
     max_tokens=VOCAB_SIZE)
 encoder.adapt(train_dataset.map(lambda text, label: text))
 
